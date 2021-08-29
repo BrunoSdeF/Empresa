2c799cd722d2c804f3bed29d29026683f4c2217a
Bruno Freitas <brunosdef10@gmail.com>
2021-08-28 21:02:08 -0300

    Atualizaçõs

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+window.addEventListener('scroll', function() {
+    // const main = document.querySelector('main').style.transform = "translateY(-100px)";;
+    // scrollX.transform = "translateY(-100px)";
+    console.log(this.scrollY);
+
+    // const main = document.querySelector('main');
+    // const span1 = document.querySelector('main').children[0].children[0].children[0];
+    // const span2 = document.querySelector('main').children[0].children[0].children[1];
+    // const span3 = document.querySelector('main').children[0].children[0].children[2];
+
+    // if (window.scrollY > 100) {
+    //     span1.style.transform = "translateY(-100px)";
+    //     span1.style.opacity = '0';
+    //     span1.style.transition = '.5s';
+    // } else {
+    //     span1.style.transform = "translateY(0px)";
+    //     span1.style.opacity = '1';
+    //     span1.style.transition = '.5s';
+    // }
+
+    // if (window.scrollY > 185) {
+    //     span2.style.transform = "translateY(-100px)";
+    //     span2.style.opacity = '0';
+    //     span2.style.transition = '.5s';
+    // } else {
+    //     span2.style.transform = "translateY(0px)";
+    //     span2.style.opacity = '1';
+    //     span2.style.transition = '.5s';
+    // }
+
+    // if (window.scrollY > 270) {
+    //     span3.style.transform = "translateY(-100px)";
+    //     span3.style.opacity = '0';
+    //     span3.style.transition = '.5s';
+    // } else {
+    //     span3.style.transform = "translateY(0px)";
+    //     span3.style.opacity = '1';
+    //     span3.style.transition = '.5s';
+    // }
+})
+
+// window.scroll(0, 0, 'smooth', behavior = 'smooth');
+// const form = document.querySelector('form');
+// form.addEventListener("click", (formName));
+
+// const nomeid = document.querySelector('#nomeid');
+
+// function formName() {
+//     if (nomeid.value.length == 0) {
+//         nomeid.focus();
+//     }
+// }
